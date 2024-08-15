08ac2cf1b659d4f64026eeadf0f9f16aae4a7763
coding-ia <137450628+coding-ia@users.noreply.github.com>
2024-08-14 23:00:16 -0700

    Update start-httpd.sh

--- scripts/start-httpd.sh
+++ scripts/start-httpd.sh
@@ -3,4 +3,4 @@ set -e
 
 mkdir ./scripts/files
 dd if=/dev/zero of=./scripts/files/file.dat bs=1024 count=2048
-docker-compose -f ./scripts/docker-compose.yaml up -d
+docker compose -f ./scripts/docker-compose.yaml up -d
